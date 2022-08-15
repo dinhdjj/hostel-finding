0113ac14a30876c3855b67a9a91709eacafe4a09
dinhdjj <dinhdjj@gmail.com>
2022-08-15 15:23:14 +0700

    build: install follow tutorial

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,13 +5,11 @@ declare(strict_types=1);
 namespace Tests;
 
 use Http;
-use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
-    use LazilyRefreshDatabase;
 
     protected function setUp(): void
     {
