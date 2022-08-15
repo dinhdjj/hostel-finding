f3c42aa669480a295b31ef241aafb7ac9320828f
dinhdjj <dinhdjj@gmail.com>
2022-08-15 14:40:27 +0700

    test: lazy refresh database setup

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,11 +5,13 @@ declare(strict_types=1);
 namespace Tests;
 
 use Http;
+use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    use LazilyRefreshDatabase;
 
     protected function setUp(): void
     {
