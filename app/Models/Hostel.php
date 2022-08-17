6cee863b05acc5f5fc799e93ec8396b104bc45ea
dinhdjj <dinhdjj@gmail.com>
2022-08-17 16:25:50 +0700

    chore: setup Amenity model

--- app/Models/Hostel.php
+++ app/Models/Hostel.php
@@ -8,6 +8,7 @@ use App\Enums\HostelStatus;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Spatie\Sluggable\HasSlug;
 use Spatie\Sluggable\SlugOptions;
 
@@ -54,5 +55,10 @@ class Hostel extends Model
     public function owner(): BelongsTo
     {
         return $this->belongsTo(User::class, 'owner_id');
+    }
+
+    public function amenities(): BelongsToMany
+    {
+        return $this->belongsToMany(Amenity::class);
     }
 }
