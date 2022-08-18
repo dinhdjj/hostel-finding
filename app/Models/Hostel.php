00ec51dd893f9a5db1013cd057fba35f8315beb5
dinhdjj <dinhdjj@gmail.com>
2022-08-18 15:36:47 +0700

    chore: use spatie/laravel-medialibrary

--- app/Models/Hostel.php
+++ app/Models/Hostel.php
@@ -11,13 +11,16 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\Sluggable\HasSlug;
 use Spatie\Sluggable\SlugOptions;
 
-class Hostel extends Model
+class Hostel extends Model implements HasMedia
 {
     use HasFactory;
     use HasSlug;
+    use InteractsWithMedia;
     use Visitable;
 
     protected $fillable = [
@@ -78,5 +81,13 @@ class Hostel extends Model
     public function votes(): HasMany
     {
         return $this->hasMany(Vote::class);
+    }
+
+    public function registerMediaCollections(): void
+    {
+        $this->addMediaCollection('default')
+            ->acceptsMimeTypes(['image/jpg', 'image/jpeg', 'image/png', 'image/bmp', 'image/gif', 'image/svg+xml', 'image/webp'])
+            ->withResponsiveImages()
+        ;
     }
 }
